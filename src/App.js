a4379d9e7acab5e6436919e55ded3b45a951a5d8
Charlie Harte <charlieharte@Charlies-MBP.attlocal.net>
2023-06-15 20:06:18 -0400

    added edit and delete functions

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
-import { useState,useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import Bread from './Components/Bread'
+import New from './Components/New'
+import UpdateBread from './Components/UpdateBread'
 
 function App() {
-const [breads, setBreads] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const URL= `${process.env.REACT_APP_BACKEND_URI}/breads`
-      const response = await fetch(URL)
-      const data = await response.json()
-      console.log(data)
-      setBreads(data)
-    }
-
-    fetchData()
-  }, [])
-
-  const display = breads.map(bread => {
-    return (
-      <div key={bread._id}>
-        <p>{bread.name}</p>
-      </div>
-    )
-  })
-
   return (
-   <div>
-    <h1>BreadCRUD</h1>
-    {display}
-   </div>
-  );
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/bread/:id' element={<Bread />} />
+          <Route path='/bread/update/:id' element={<UpdateBread />} />
+          <Route path='/new' element={<New />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
